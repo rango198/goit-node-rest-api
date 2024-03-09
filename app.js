30abddbb45dcd2b1db26053190c82540a910dc2d
rango198 <137562811+rango198@users.noreply.github.com>
2024-03-09 20:38:01 +0200

    mongoDB

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import mongoose from "mongoose";
+
+const DB_HOST =
+  "mongodb+srv://DataBase:23pm89ecio@cluster0.5efshsq.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connect success");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
 import contactsRouter from "./routes/contactsRouter.js";
+import { error } from "console";
 
 const app = express();
 
@@ -21,6 +41,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
